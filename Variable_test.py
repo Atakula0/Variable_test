c05e38ce2e7b374a16b53724052750a2adb3fd34
Atakula0 <154466360+Atakula0@users.noreply.github.com>
2024-01-08 21:53:36 -0500

    Add files via upload

---
+++ Variable_test.py
@@ -0,0 +1,33 @@
+#variable test
+
+first_variable = "X"
+second_variable = "Y"
+complete = first_variable +" "+ second_variable
+print("hello "+complete)
+
+numerical_variable = 15
+numerical_variable = numerical_variable + 15
+print(numerical_variable)
+
+#shortcut/class displayed
+
+numerical_variable = 15
+numerical_variable +=15
+print(type(numerical_variable))
+
+#concatenating a string and integer
+
+numerical_variable = 15
+numerical_variable +=15
+print("total value is: "+str(numerical_variable))
+
+#floating point integer test
+
+floating_point = 15.5
+print("the floating point value is:"+str(floating_point))
+print(type(floating_point))
+
+#boolean value test
+
+True_value = True
+print("Is this value true: "+str(True_value))
